4765c3069d2491f3d570eaed78b64ea1ece40f89
prithviie <prithvippv25@gmail.com>
2021-04-14 23:54:28 +0530

    added board generator in sudoku_solver.py

--- sudoku_solver/README.md
+++ sudoku_solver/README.md
@@ -1,1 +1,4 @@
 # Sudoku solver using the Backtracking algorithm
+
+Generates a random sudoku board and solves it
+
--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,17 +1,7 @@
-
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
+import random
 
 def print_board(bo):
+    print()
     count = 0
     for i in range(len(bo)):
         for j in range(len(bo[0])):
@@ -23,14 +13,18 @@ def print_board(bo):
             if j % 3 == 0 and j != 0:
                 print('| ', end='')
 
-            print(str(bo[i][j]) + ' ', end='')
-            # print(str((i, j)) + ' ', end='')
+            if bo[i][j] == 0:
+                print('- ', end='')
+            else:
+                print(f'{bo[i][j]} ', end='')
 
         count += 1
         print()
+    print()
 
 
 def find_empty(bo):
+    '''finds an empty spot in the board and returns its position'''
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
@@ -40,53 +34,37 @@ def find_empty(bo):
 
 
 def is_valid(bo, num, pos):
+    '''checks if the num is valid to insert into the current state of the board'''
 
     x, y = pos
 
-    # bo[x][y] = '*'
-    # print_board(bo)
-
-    # print()
-    # print('num -->', str(num))
-    
-    # print('pos --> ', end='')
-    # print(x, y)
-    # print()
-
-
-    # check row
-
+    # checking if pos is valid in the row
     row = bo[x]
     if num in row:
-        # print('row', end=' --> ')
-        # print(row)
         return False
 
 
-    # check col
-
+    # checking if pos is valid in the column
     col = [bo[i][y] for i in range(len(bo[0]))]
     if num in col:
-        # print('col', end=' --> ')
-        # print(col)
         return False
 
 
-    # check box
-
+    # checking if pos is valid in box
     box_x = x // 3
     box_y = y // 3
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x*3, box_x*3 + 3):
             if num == bo[j][i]:
-                # print('box', end=' --> ')
                 return False
 
+    # if valid in all three conditions, return True
     return True
 
 
 def solve(bo):
+    '''solves the board recursively using backtracking'''
     
     find = find_empty(bo)
 
@@ -95,6 +73,7 @@ def solve(bo):
     else:
         row, col = find
 
+    # try inserting every value in range 1 to 9
     for i in range(1, 10):
         if is_valid(bo, i, (row, col)):
             bo[row][col] = i
@@ -102,14 +81,40 @@ def solve(bo):
             if solve(bo):
                 return True
 
+            # if not solved empty the position
             bo[row][col] = 0
 
     return False
 
 
-print_board(board)
-solve(board)
-print()
-print('---------------------')
-print()
-print_board(board)
+def generate_board():
+    '''returns a new unsolved board'''
+
+    new_board = []
+    
+    for i in range(9):
+        new_board.append([])
+        for j in range(9):
+            new_board[i].append(0)
+
+    solve(new_board)
+
+    for i in range(45):
+        x, y = random.randint(0, 8), random.randint(0, 8)
+        new_board[x][y] = 0
+
+    return new_board
+
+
+def main():
+    
+    board = generate_board()
+    print_board(board)
+
+    input('Hit ENTER to solve! ')
+
+    solve(board)
+    print_board(board)
+
+
+main()
