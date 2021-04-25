b99b1071a10c1ddf4cd34a3f20b66d163ccc84f1
prithviie <prithvippv25@gmail.com>
2021-04-25 11:07:57 +0530

    directory_tree in working state

--- directory_tree_generator/tree.py
+++ directory_tree_generator/tree.py
@@ -10,10 +10,11 @@ SPACE_PREFIX = "    "
 class Dir:
 
     def __init__(self, root_dir):
+        global initial_dir_level
         self.root_dir = root_dir
         self.files = []
         self.dirs = []
-        self.level = self.root_dir.count('/')
+        self.level = self.root_dir.count('/') - initial_dir_level
 
         for f in os.listdir(self.root_dir):
             temp_path = f"{self.root_dir}/{f}"
@@ -27,31 +28,28 @@ class Dir:
 
         basename = self.root_dir.split('/')[-1]
         r = ""
-        if basename == root_folder and self.level == 0:
-            r += f"{basename}/\n"
-            r += "│\n"
+        if self.level == 0:
+            r += f"{basename}/\n│\n"
+        elif len(self.files) == 0:
+            r += f" {basename}/"
         else:
             r += f" {basename}/\n"
 
         if len(self.dirs) > 0:
             for i in range(len(self.dirs)):
-                r += self.level * PIPE_PREFIX
-                r += f"{TEE}{self.dirs[i]}\n"
+                r += self.level * PIPE_PREFIX + f"{TEE}{self.dirs[i]}\n"
 
         for i in range(len(self.files)):
             if i == len(self.files)-1:
-                r += self.level * PIPE_PREFIX
-                r += f"{ELBOW} {self.files[i]}"
-                r += f"\n{self.level*PIPE_PREFIX}"
+                r += self.level * PIPE_PREFIX + f"{ELBOW} {self.files[i]}" + f"\n{self.level*PIPE_PREFIX}"
             else:
-                r += self.level * PIPE_PREFIX
-                r += f"{TEE} {self.files[i]}\n"
+                r += self.level * PIPE_PREFIX + f"{TEE} {self.files[i]}\n"
 
         return r
 
 
 root_folder = 'directory_tree_generator/hello'
+initial_dir_level = root_folder.count('/')
+
 root = Dir(root_folder)
 print(root)
-
-# check level settings
